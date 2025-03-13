8ff98f0cb212a60fc416e9695ffa9b2d83af5d85
fattahali37 <fattahali37@gmail.com>
2025-03-13 23:39:17 +0500

    .

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import LoginLogo from "./Login.png";
+import loginLogo from "./Login.png";
 
 interface SignupProps {
   onSignup: () => void;
